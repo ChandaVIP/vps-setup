2a6554a800dc11ed3f068110992c76c67912e82f
ChandaVIP <93756205+ChandaVIP@users.noreply.github.com>
2026-08-08 06:38:27 +0700

    Update vps-setup.sh

--- vps-setup.sh
+++ vps-setup.sh
@@ -1,127 +1,58 @@
-#!/bin/bash
-# Setup a Debian Virtual Private Server (VPS) hosted in Hetzner
-# This script follows the guidelines recommended by Hetzner:
-#  - https://community.hetzner.com/tutorials/setup-ubuntu-20-04
-#
-# This script is developed for Debian 12.
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-RESET='\033[0m'
+#!/usr/bin/env python3
+import socket, threading, ssl
 
-if [ -z "$1" ] || [ "$1" != "--confirm" ]; then
-    echo "ATTENTION!!"
-    echo "This command will disable password authentication and root login."
-    echo "This means that once you logout from the current session you will no longer be able to login again as root."
-    echo "After running make sure you can login with the newly created sysadmin user BEFORE closing this session."
-    echo ""
-    echo "If you understand this, execute the command with --confirm argument."
-    exit 1
-fi
+LISTEN_PORT = 80
+SSH_ADDR = '127.0.0.1'
+SSH_PORT = 22
 
-if [ ! -f id_ed25519.pub ]; then
-    echo -e "${RED}This script will create a sysadmin account. A file named id_ed25519.pub with the public key for this user needs to exist in this directory.${RESET}"
-    echo -e "${RED}Exiting...${RESET}"
-    exit 1
-fi
+CERT_FILE = '/etc/letsencrypt/live/chanda-vpn.duckdns.org/fullchain.pem'
+KEY_FILE = '/etc/letsencrypt/live/chanda-vpn.duckdns.org/privkey.pem'
 
-update_apt() {
-    echo -e "${GREEN} Updating apt-get repository and upgrading the system...${RESET}"
-    apt-get update -qq
-    apt-get upgrade -qq
-}
+def handle_client(client_socket):
+    try:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(certfile=CERT_FILE, keyfile=KEY_FILE)
+        secure_socket = context.wrap_socket(client_socket, server_side=True)
 
-setup_firewall() {
-  echo -e "${GREEN}Installing firewall and opening only ports 1222 (ssh), 80 and 443... ${RESET}"
-  apt-get install ufw -qq
-  ufw default deny incoming
-  ufw allow 1222/tcp
-  ufw allow 80/tcp
-  ufw allow 443/tcp
-  ufw enable
-}
+        request = secure_socket.recv(4096)
+        if not request:
+            secure_socket.close()
+            return
 
-setup_ssh_daemon() {
-  echo -e "${GREEN}Disabbling password authentication, root login and changing SSH port to 1222...${RESET}"
-  sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-  # prohibit-password is Debian's default
-  sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
-  sed -i 's/#Port 22/Port 1222/' /etc/ssh/sshd_config
-  systemctl restart ssh
-}
+        ssh_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssh_socket.connect((SSH_ADDR, SSH_PORT))
 
-setup_fail2ban() {
-  echo -e "${GREEN}Installing and enabling fail2ban (using default configs)...${RESET}"
-  apt-get install fail2ban -qq
-  # For Debian 12 install: https://github.com/fail2ban/fail2ban/issues/3292#issuecomment-1678844644
-  # This should no longer be necessary in the next 1.1.1 release: https://github.com/fail2ban/fail2ban/commit/d0d07285234871bad3dc0c359d0ec03365b6dddc
-  apt-get install python3-systemd
-  echo -e "[sshd]\nbackend=systemd\nenabled=true" | sudo tee /etc/fail2ban/jail.local
-  systemctl enable fail2ban
-  systemctl start fail2ban
-}
+        secure_socket.sendall(b"HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\n\r\n")
 
-setup_logwatch() {
-  echo -e "${GREEN}Installing and enabling logwatch (using default configs)...${RESET}"
-  apt-get install logwatch -qq
-}
+        def forward(src, dst):
+            try:
+                while True:
+                    data = src.recv(4096)
+                    if not data: break
+                    dst.sendall(data)
+            except:
+                pass
+            finally:
+                try: src.close()
+                except: pass
+                try: dst.close()
+                except: pass
 
-install_utils() {
-  echo -e "${GREEN}Installing util tools: htop, vim and git...${RESET}"
-  apt-get install htop vim git -qq
-}
+        threading.Thread(target=forward, args=(secure_socket, ssh_socket)).start()
+        threading.Thread(target=forward, args=(ssh_socket, secure_socket)).start()
+    except:
+        try: client_socket.close()
+        except: pass
 
-add_sysadmin_user() {
-  echo -e "${GREEN}Adding a sysadmin user with sudo permission...${RESET}"
-  adduser sysadmin
-  usermod -aG sudo sysadmin
-  mkdir /home/sysadmin/.ssh
-  echo -e "${GREEN}Adding the id_ed25519.pub key to authorized_keys file of sysadmin user...${RESET}"
-  cat id_ed25519.pub >> /home/sysadmin/.ssh/authorized_keys
-}
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('0.0.0.0', LISTEN_PORT))
+    server.listen(100)
+    
+    while True:
+        client_sock, _ = server.accept()
+        threading.Thread(target=handle_client, args=(client_sock,)).start()
 
-finish_message() {
-  echo -e "${GREEN}Configuration Finished!! Before closing this session check that you can ssh in using port 1222 with the newly created sysadmin account.${RESET}"
-}
-
-install_docker() {
-  echo -e "${GREEN}Installing latest version of docker...${RESET}"
-  # Commands extracted from official documentation:
-  # https://docs.docker.com/engine/install/debian/#install-using-the-repository
-  apt-get update -qq
-  apt-get install ca-certificates curl -qq
-  install -m 0755 -d /etc/apt/keyrings
-  curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-  chmod a+r /etc/apt/keyrings/docker.asc
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
-  apt-get update -qq
-  apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -qq
-
-  echo -e "${GREEN}Adding sysadmin user to docker group...${RESET}"
-  usermod -aG docker sysadmin
-}
-
-unattended_upgrades() {
-  # Install and enable unattended-upgrades for automatic security upgrades.
-  # https://wiki.debian.org/PeriodicUpdates?action=show&redirect=UnattendedUpgrades
-  echo -e "${GREEN}Ensuring unattended-upgrades is installed and running...${RESET}"
-  apt-get install unattended-upgrades -qq
-  dpkg-reconfigure -f noninteractive unattended-upgrades
-}
-
-main () {
-    update_apt
-    setup_firewall
-    setup_ssh_daemon
-    setup_fail2ban
-    setup_logwatch
-    install_utils
-    add_sysadmin_user
-    install_docker
-    unattended_upgrades
-    finish_message
-}
-
-main
+if __name__ == '__main__':
+    main()
